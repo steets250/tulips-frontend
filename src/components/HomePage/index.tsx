ba0dc20eb271f03fdcb66ab7e5fb683eb9bc7252
Steven Steiner <steets250@gmail.com>
2022-03-13 20:03:02 -0700

    Home Page Updates

--- src/components/HomePage/index.tsx
+++ src/components/HomePage/index.tsx
@@ -4,54 +4,51 @@ import StoryCard from '../StoryCard';
 
 import './style.css';
 
+interface Story {
+    uuid: string;
+    text: string;
+    title: string;
+    likes: number;
+}
+
+const stories: Story[] = [
+    {
+        uuid: "1",
+        text: "Whenever I try to pitch an idea in a group setting, I feel unnecessarily interrogated about the details of the idea, as if people are purposely trying to pick out flaws so they don't have to use the idea. At first I thought people just wanted to be thorough and didn't think much, but then I've noticed that it's only with the women. Guys in the group don't face the same treatment because any idea they have is met with instant respect.",
+        title: "Gary Copypasta",
+        likes: 21
+    },
+    {
+        uuid: "2",
+        text: "I have seniority in a STEM program, yet most members still don't respect my advice.",
+        title: "Gary Copypasta",
+        likes: 11
+    },
+    {
+        uuid: "3",
+        text: "Most of the guys that I meet in my classes or orgs don't really see me as another student to be friends with. It feels that they see me only as a potential date.",
+        title: "Gary Copypasta",
+        likes: 19
+    },
+    {
+        uuid: "4",
+        text: "I'm tired of fighting for myself and having to prove that I'm just as competent in my field as other men.",
+        title: "Gary Copypasta",
+        likes: 16
+    },
+    {
+        uuid: "5",
+        text: "I feel like I don't fit in with a lot of student groups because I'm not White or Asian, and it often feels clique-y.",
+        title: "Gary Copypasta",
+        likes: 26
+    },
+];
+
 const HomePage: React.FC = () => {
     return (
         <>
         <div className="home-page-stories">
-            <StoryCard
-                uuid="deez"
-                story='When you accept an offer, the company and its people begin to rely on you, and they become invested in the positive outcome resulting from that relationship. Your future actions, whether positive or negative, rub off on those who stood beside you and this is with UCSD’s reputation with Microsoft, the CSE Department’s reputation with Microsoft, the CS Tutors’ reputations with Microsoft, and as a result Rick’s and my reputation with Microsoft does not shine as brightly due to your selfish actions.'
-                likes={420}
-                handleLike={(uuid: string) => {
-                alert("You liked " + uuid + "!")
-                }}
-                handleReport={(uuid: string) => {
-                alert("You reported " + uuid + "!")
-                }}
-            />
-            <StoryCard
-                uuid="deez"
-                story='When you accept an offer, the company and its people begin to rely on you, and they become invested in the positive outcome resulting from that relationship. Your future actions, whether positive or negative, rub off on those who stood beside you and this is with UCSD’s reputation with Microsoft, the CSE Department’s reputation with Microsoft, the CS Tutors’ reputations with Microsoft, and as a result Rick’s and my reputation with Microsoft does not shine as brightly due to your selfish actions.'
-                likes={420}
-                handleLike={(uuid: string) => {
-                alert("You liked " + uuid + "!")
-                }}
-                handleReport={(uuid: string) => {
-                alert("You reported " + uuid + "!")
-                }}
-            />
-            <StoryCard
-                uuid="deez"
-                story='When you accept an offer, the company and its people begin to rely on you, and they become invested in the positive outcome resulting from that relationship. Your future actions, whether positive or negative, rub off on those who stood beside you and this is with UCSD’s reputation with Microsoft, the CSE Department’s reputation with Microsoft, the CS Tutors’ reputations with Microsoft, and as a result Rick’s and my reputation with Microsoft does not shine as brightly due to your selfish actions.'
-                likes={420}
-                handleLike={(uuid: string) => {
-                alert("You liked " + uuid + "!")
-                }}
-                handleReport={(uuid: string) => {
-                alert("You reported " + uuid + "!")
-                }}
-            />
-            <StoryCard
-                uuid="deez"
-                story='When you accept an offer, the company and its people begin to rely on you, and they become invested in the positive outcome resulting from that relationship. Your future actions, whether positive or negative, rub off on those who stood beside you and this is with UCSD’s reputation with Microsoft, the CSE Department’s reputation with Microsoft, the CS Tutors’ reputations with Microsoft, and as a result Rick’s and my reputation with Microsoft does not shine as brightly due to your selfish actions.'
-                likes={420}
-                handleLike={(uuid: string) => {
-                alert("You liked " + uuid + "!")
-                }}
-                handleReport={(uuid: string) => {
-                alert("You reported " + uuid + "!")
-                }}
-            />
+            {stories.map((story) => <StoryCard story={story}/>)}
         </div>
         </>
     );
--- src/components/StoryCard/index.tsx
+++ src/components/StoryCard/index.tsx
@@ -1,39 +1,135 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import PinIcon from "../../assets/pin.svg";
 import ReportIcon from "../../assets/report.svg";
-import LikeIcon from "../../assets/like.svg";
+import LikedIcon from "../../assets/liked.svg";
+import UnlikedIcon from "../../assets/unliked.svg";
 
 import './style.css';
 
-interface StoryCardProps {
+interface Story {
     uuid: string;
-    story: string;
+    text: string;
+    title: string;
     likes: number;
-    handleLike: Function;
-    handleReport: Function;
+}
+
+interface StoryCardProps {
+    story: Story;
 }
 
 const StoryCard: React.FC<StoryCardProps> = (props) => {
-    const { uuid, story, likes, handleLike, handleReport } = props;
+    const {
+        story: {
+            uuid,
+            text,
+            title,
+            likes,
+        }
+    } = props;
 
-    return (
-        <div className="story-card">
-            <img className="story-card-pin-icon" src={PinIcon} alt="Pushpin"></img>
-            <p className="story-card-story">{story}</p>
-            <div className="story-card-bottom">
-                <button className="story-card-report-button" type="button" onClick={() => handleReport(uuid)}>
-                    <img className="story-card-report-button-icon" src={ReportIcon} alt="Report" />
-                </button>
-                <div className="story-card-like-container">
-                    <p className="story-card-likes">{likes}</p>
-                    <button className="story-card-like-button" type="button" onClick={() => handleLike(uuid)}>
-                        <img className="story-card-like-button-icon" src={LikeIcon} alt="Like" />
-                    </button>
+    const [mode, setMode] = useState<"NORMAL" | "REPORTED" | "CREATING">("NORMAL");
+    const [liked, setLiked] = useState<boolean>(false);
+
+    useEffect(() => {
+        setLiked(localStorage.getItem(`${uuid}-liked`) === "true")
+    }, [setLiked, uuid]);
+
+    const handleLike = (uuid: string) => {
+        if (localStorage.getItem(`${uuid}-liked`)) {
+            // API CALL HERE
+            localStorage.removeItem(`${uuid}-liked`);
+            setLiked(false);
+            alert("You unliked " + uuid + "!");
+        } else {
+            // API CALL HERE
+            localStorage.setItem(`${uuid}-liked`, "true");
+            setLiked(true);
+            alert("You liked " + uuid + "!");
+        }
+    };
+
+    const handleReport = (uuid: string) => {
+        if (localStorage.getItem(`${uuid}-reported`)) {
+            alert("You already reported " + uuid + "!");
+        } else {
+            // API CALL HERE
+            localStorage.setItem(`${uuid}-reported`, "true");
+            alert("You reported " + uuid + "!");
+        }
+    }
+
+    const handleCreate = (title: string, text: string) => {
+        // API CALL HERE
+        alert(`You created a story with title: "${title}" and the text: "${text}"`);
+    }
+
+    let content = <></>;
+
+    switch (mode) {
+        case "NORMAL":
+            content = (
+                <div className="story-card-content">
+                    <img className="story-card-pin-icon" src={PinIcon} alt="Pushpin"></img>
+                    <p className="story-card-story">{text}</p>
+                    <div className="story-card-bottom">
+                        <button className="story-card-report-button" type="button" onClick={() => handleReport(uuid)}>
+                            <img className="story-card-report-button-icon" src={ReportIcon} alt="Report" />
+                        </button>
+                        <div className="story-card-like-container">
+                            <p className="story-card-likes">{likes}</p>
+                            <button className="story-card-like-button" type="button" onClick={() => handleLike(uuid)}>
+                                <img className="story-card-like-button-icon" src={liked ? LikedIcon : UnlikedIcon} alt="Like" />
+                            </button>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-    );
+            );
+            break;
+
+        case "REPORTED":
+            content = (
+                <>
+                    <img className="story-card-pin-icon" src={PinIcon} alt="Pushpin"></img>
+                    <div className="story-card-content reported">
+                        <p className="story-card-story">{text}</p>
+                        <div className="story-card-bottom">
+                            <button className="story-card-report-button" type="button" onClick={() => handleReport(uuid)}>
+                                <img className="story-card-report-button-icon" src={ReportIcon} alt="Report" />
+                            </button>
+                            <div className="story-card-like-container">
+                                <p className="story-card-likes">{likes}</p>
+                                <button className="story-card-like-button" type="button" onClick={() => handleLike(uuid)}>
+                                    <img className="story-card-like-button-icon" src={UnlikedIcon} alt="Like" />
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="story-card-report">
+                        <p className="story-card-report-text">You have reported this story.<br/>It will be reviewed by a site admin.</p>
+                    </div>
+                </>
+            );
+            break;
+
+        case "CREATING":
+            content = (
+                <div className="story-card-content">
+                    <img className="story-card-pin-icon" src={PinIcon} alt="Pushpin"></img>
+                    <textarea className="story-card-input" />
+                    <div className="story-card-bottom">
+                        <p className="story-card-terms">By sharing, you agree to the <a href="" className="underline">terms</a>.</p>
+                        <button className="story-card-button" onClick={() => handleCreate("one", "two")}>Share</button>
+                    </div>
+                </div>
+            );
+            break;
+    
+        default:
+            break;
+    }
+
+    return <div className="story-card">{content}</div>;
 }
 
 export default StoryCard;
